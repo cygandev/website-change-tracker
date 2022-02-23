879260260b990b9b4ff8a19bfb2bae9821ead43a
cygandev <cygandev@protonmail.com>
2022-02-23 17:18:12 +0100

    send html email

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ from utils.handle_content import (
 from utils.send_mail import send_mail
 
 URL = os.environ.get("URL")
-FILE_PATH = os.environ.get("FILE_PATH", "./website_content.txt")
+WEBPAGE_CONTENT_PATH = os.environ.get("WEBPAGE_CONTENT_PATH", "./files/website_content.txt")
+HTML_MESSAGE_PATH = os.environ.get("HTML_MESSAGE_PATH", "./files/msg_content.html")
 
 GMAIL_ADR = os.environ.get("EMAIL")
 GMAIL_PW = os.environ.get("PW_GMAIL")
@@ -20,24 +21,28 @@ EMAIL_RECEIVER = os.environ.get("EMAIL_RECEIVER", GMAIL_ADR)
 if __name__ == "__main__":
     new_page_content = request_page_content(URL)
     try:
-        old_page_content = read_page_content(FILE_PATH)
+        old_page_content = read_page_content(WEBPAGE_CONTENT_PATH)
         if old_page_content == new_page_content:
             print("INFO: Webpage scraped, no changes detected")
         else:
             msg_subject = "ALERT - WEBSITE CHANGE DETECTED"
-            msg_content = f"A change on the webpage {URL} was detected \
+            msg_content_plain = f"A change on the webpage {URL} was detected \
             please visit page."
+            with open(HTML_MESSAGE_PATH, 'r') as f:
+                msg_content_html = f.read().format(OBSERVED_URL=URL)
+
             send_mail(
                 GMAIL_ADR,
                 GMAIL_PW,
-                GMAIL_ADR,
-                GMAIL_ADR,
+                EMAIL_SENDER,
+                EMAIL_RECEIVER,
                 message_subject=msg_subject,
-                message_content=msg_content,
+                message_content_plain=msg_content_plain,
+                message_content_html=msg_content_html,
             )
             print(f"INFO: Change detected, email to {EMAIL_RECEIVER} sent...")
     except FileNotFoundError:
         print("INFO: No file found, initiating...")
     finally:
-        write_page_content(FILE_PATH, new_page_content)
+        write_page_content(WEBPAGE_CONTENT_PATH, new_page_content)
         print("INFO: Successfully Written new page content to disk")
--- utils/send_mail.py
+++ utils/send_mail.py
@@ -1,5 +1,7 @@
 import smtplib
 from email.message import EmailMessage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 import ssl
 
 
@@ -9,7 +11,8 @@ def send_mail(
     email_sender: str,
     email_receiver: str,
     message_subject: str,
-    message_content: str,
+    message_content_plain: str,
+    message_content_html: str,
     host_ssl_port: int = 465,
 ) -> None:
     """Sends email with defined content using defined host (e.g. gmail)
@@ -18,7 +21,7 @@ def send_mail(
         host_email_adr (str): email address of host used to send mails
         host_email_pw (str): password of host email address
         email_sender (str): receiver email address
-        email_receiver (str): sender email address
+        email_receiver (str): sender email address. For multiple receipients use "email1@gm.com, email2@gmail.com"
         message_subject (str): message header
         message_content (str): message body
         host_ssl_port (int, optional): ssl port of host. Defaults to 465.
@@ -26,11 +29,17 @@ def send_mail(
     Returns:
         None: None
     """
-    msg = EmailMessage()
-    msg.set_content(message_content)
+    msg = MIMEMultipart('alternative')
     msg["Subject"] = message_subject
     msg["From"] = email_sender
     msg["To"] = email_receiver
+    email_receiver_list = email_receiver.replace(" ", "").split(",")
+
+    content_plain = MIMEText(message_content_plain, 'plain')
+    content_html = MIMEText(message_content_html, 'html')
+
+    msg.attach(content_plain)
+    msg.attach(content_html)
 
     # Create a secure SSL context
     context = ssl.create_default_context()
@@ -38,6 +47,6 @@ def send_mail(
         "smtp.gmail.com", host_ssl_port, context=context
     ) as server:
         server.login(host_email_adr, host_email_pw)
-        server.send_message(msg)
+        server.sendmail(email_sender,email_receiver_list,msg.as_string())
     print("INFO: email sent")
     return None
